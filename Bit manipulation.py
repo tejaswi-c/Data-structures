f9900ff4b81fa72ca7a42172935e261d980356d9
tejaswi-c <85658141+tejaswi-c@users.noreply.github.com>
2023-06-30 16:03:05 +0530

    Update Bit manipulation.py

--- Bit manipulation.py
+++ Bit manipulation.py
@@ -99,3 +99,92 @@ A=1010=10
 B=10100=20
 C=A^B =11110=30
 output=4
+
+6.Count total set bits in all numbers from 1 to n
+App:
+-find total number of set bits in a number
+-loop for each element from 1 to n
+def total_setbits(n):
+    count=0
+    for i in range(1,n+1):
+    	while i:
+    	   i&=(i-1)
+    	   count+=1
+    return count
+TC:O(n^2)
+
+App:Optimized 
+-initialize total_count=0
+-initialize i to 1
+-enter while i<=n
+-calculate quotient-number of complete groups of 2*i bits
+-calculate remainder-remaining bit
+-add count by complete groups to total_count
+-add count by remaining bits to total_count
+-left shift i by 1 to move to next position
+-repeat until i<=n
+-return total_count
+
+def countsetbits_1_to_n(n):
+ total_count = 0
+    i = 1
+    while i <= n:
+        quotient = n // (i << 1)  
+        remainder = n % (i << 1) 
+        total_count += quotient * i
+        if remainder >= i:
+            total_count += remainder - i + 1
+        i <<= 1  
+    return total_count
+7.
+8.Calculate square of a number without using *, / and pow()
+App1:repeated addition
+def square_number(n):
+	if n==0:
+		return 0
+	if n<0:
+		n=-n
+	result=0
+	for i in range(n):
+		result+=n
+	return result
+TC:O(n)
+SC:O(1)
+
+App2:
+-calculate n//2 recursively and store in half
+-if n is even return 4*half
+-else return 4*half+(n//2)+(n//2)+1
+def square_number(n):
+	if n==0:
+		return 0
+	if n<0:
+		n=-n
+	if n%2==0:
+		half=square_number(n//2)
+		return 4*half
+	else:
+		half=square_number(n//2)
+		return 4*half+(n//2)+(n//2)+1
+TC:O(logn)
+SC:O(logn)
+
+9.
+10.Power Set
+-generate all combination by iterating 2*n possible sets
+-(i >> j) & 1 checks if the j-th bit of the current combination 
+i is set (non-zero). If it is, it means that the j-th 
+character from the input string should be included in 
+the current subset.
+def generate_power_set(nums):
+	n=len(s)
+	power_set=[]
+	for i in range(2**n):
+	    subset=[]
+		for j in range(n):
+			if(i>>j)&1:
+				subset.append(s[j])
+		x=''.join(subset)
+		power_set.append(x)
+	power_set.sort()
+    return power_set[1:]
