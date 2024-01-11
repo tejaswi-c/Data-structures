fb9e85aa92e247bf10b96a0e97abcb69e0a69247
tejaswi-c <85658141+tejaswi-c@users.noreply.github.com>
2024-01-11 22:26:56 +0530

    Update practise.py

--- practise.py
+++ practise.py
@@ -33,4 +33,42 @@ class Solution(object):
             fast=fast.next
         slow.next=slow.next.next 
         return dummy.next
-        
+
+
+3.Combinations
+def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        def backtrack(start,path):
+            if len(path)==k:
+                result.append(path[:])
+                return 
+            for i in range(start,n+1):
+                path.append(i)
+                backtrack(i+1,path)
+                path.pop()
+        result=[]
+        backtrack(1,[])
+        return result 
+
+4.permutations
+def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def backtrack(path):
+            if len(path)==len(nums):
+                result.append(path[:])
+                return
+            for num in nums:
+                if num not in path:
+                    path.append(num)
+                    backtrack(path)
+                    path.pop()
+        result=[]
+        backtrack([])
+        return result     
