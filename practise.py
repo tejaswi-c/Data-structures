df80a444a6cf2e748035c9349ffba8633226b5f9
tejaswi-c <85658141+tejaswi-c@users.noreply.github.com>
2024-01-14 22:41:16 +0530

    Update practise.py

--- practise.py
+++ practise.py
@@ -71,4 +71,34 @@ def permute(self, nums):
                     path.pop()
         result=[]
         backtrack([])
-        return result     
+        return result   
+
+5.Merge sorted array
+
+
+
+
+Algorithm:
+-Traverse from right to left 
+-create two pointers for first array and one for second
+ -loop:
+    if element of first array is greater then add it to first array
+    else add it to the next array.
+
+def merge(self, nums1, m, nums2, n):
+        p1=m-1
+        p2=n-1
+        k=m+n-1
+        while p1>=0 and p2>=0:
+            if nums1[p1]>nums2[p2]:
+                nums1[k]=nums1[p1]
+                p1-=1
+            else:
+                nums1[k]=nums2[p2]
+                p2-=1
+            k-=1
+        if p2>=0:
+            nums1[:k+1]=nums2[:p2+1]
+
+
+
