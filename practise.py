ac4f7ae9f7180a69920d5cc395046e8feeaac759
tejaswi-c <85658141+tejaswi-c@users.noreply.github.com>
2024-01-17 11:45:55 +0530

    Update practise.py

--- practise.py
+++ practise.py
@@ -123,5 +123,18 @@ def removeElement(self, nums, val):
 
 
 
-
+3.Remove duplicates from the sorted array
+ def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        duplicates=0
+        for i in range(1,len(nums)):
+            if nums[i]==nums[i-1]:
+                duplicates+=1
+            else:
+                nums[i-duplicates]=nums[i]
+        return len(nums)-duplicates
+                                           
 
