478e292393d92c670276206c5991179b0c643fe4
tejaswi-c <85658141+tejaswi-c@users.noreply.github.com>
2024-01-29 20:40:39 +0530

    Update practise.py

--- practise.py
+++ practise.py
@@ -365,4 +365,18 @@ Code:
 
 Time complexity:O(n)
 Space complexity:O(n)
-	 
+
+
+15.Roman to Integer
+def romanToInt(self, s):
+        roman_to_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        prev_value = 0
+
+        for char in s:
+            current_value = roman_to_int[char]
+            result += current_value
+            if current_value > prev_value:
+                result -= 2 * prev_value
+            prev_value = current_value
+        return result
